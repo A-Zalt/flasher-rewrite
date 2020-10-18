07eb238ab18f698ea634c063fd15c648fe382126
tuxlabore <tls.tuxla@gmail.com>
2020-10-18 17:57:59 +0300

    + core.files.load_locales

--- core/files.py
+++ core/files.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from logging import debug, info
+from logging import debug, info, warn
 import yaml
 import os
 
@@ -13,7 +13,7 @@ def load(path: str='config.yaml'):
     file = Path(path).open()
     result = yaml.safe_load(file)
 
-    info(f'YAML file {path} loaded and parsed succesful')
+    debug(f'YAML file {path} loaded and parsed succesful')
 
     return result
 
@@ -25,3 +25,39 @@ def change_environment_variables():
         os.environ[key] = values[key]
 
     info(f'Changed environment variables to {values}')
+
+def load_locales(folder: str='locale'):
+    """Loads .yaml localization files.
+
+    Arguments
+    ---------
+    folder: str = 'locale' - Folder with localization files
+
+    Returns
+    ---------
+    Dictionary with localizations. Example:
+    ```
+    {
+    'ru_RU':{'language.name.full': 'Русский (Россия)'},
+    'en_US':{'language.name.full': 'English (United States)'}
+    }
+    ```"""
+    files = {}
+    locales = {}
+    for file in os.scandir(folder):
+        files[file.path] = file.name.replace('.yaml','') # 'locale/en_US.yaml' : 'en_US'
+
+    for path in files.keys():
+        try:
+            result = load(path)
+            
+            if not isinstance(result, dict):
+                raise TypeError('The localization file is not created correctly (dictionary excepted)')
+
+            locales[files[path]] = result # locales['en_US'] : {}
+        except Exception as e:
+            warn(f'Failed to load "{path}" locale. {type(e).__name__}: {str(e)}')
+        else:
+            info(f'Locale {path} succesfully loaded')
+
+    return locales
